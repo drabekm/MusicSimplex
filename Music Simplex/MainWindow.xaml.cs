5a3dd1301cf94a0380fb867dc850df084e984a2b
Matěj Drábek <matej1drabek@seznam.cz>
2020-05-08 20:58:42 +0200

    Add test usercontrol
    Slowly figuring out how wpf actually works. Yay I'm learning

--- Music Simplex/MainWindow.xaml.cs
+++ Music Simplex/MainWindow.xaml.cs
@@ -13,16 +13,43 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Music_Simplex
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        MediaPlayer player = new MediaPlayer();
+
+      
+
         public MainWindow()
         {
             InitializeComponent();
+            player.MediaFailed += (o, args) =>
+            {
+                MessageBox.Show("Media Failed!!");
+            };
+            MyUserControl.myEvent += MyEventHandler;
+            
+            //player.Open(new Uri(@"C:\programovani\MusicSimplex\Music Simplex\bin\Debug\Romashki.mp3", UriKind.Absolute));
+          /*  player.Open(new Uri("testmusic.mp3", UriKind.Relative));
+            player.Play();
+            var position = player.Position;
+
+            MessageBox.Show(position.ToString() + " : " + player.NaturalDuration);*/
+
         }
+
+        static void MyEventHandler(object sender, EventArgs e)
+        {
+            var myEventArg = (MyEventArg)e;
+            
+            MessageBox.Show(myEventArg.Content);
+        }
+
     }
 }
---
+++ Music Simplex/MyEventArg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_Simplex
+{
+    class MyEventArg : EventArgs
+    {
+        public MyEventArg(string content) : base()
+        {
+            this.Content = content;
+        }
+
+        public string Content { get; private set; }
+    }
+}
